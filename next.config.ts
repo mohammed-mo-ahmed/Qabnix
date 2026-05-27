71e25bde216722f872d758d02f96e4a69bdaddf3
Mohammed Ahmed <mohammed.mo.alkabbani@gmail.com>
2026-05-27 19:57:44 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // هذا سيجعل المشروع يخرج ملفات ثابتة فقط
-  images: {
-    unoptimized: true, // ضروري لأن نظام الصور في Next يحتاج سيرفر
-  },
+  output: 'export',
 }
+module.exports = nextConfig
