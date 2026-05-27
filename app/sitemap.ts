c8f83f532d822e3188ee82949b735efb974dc3e0
Mohammed Ahmed <mohammed.mo.alkabbani@gmail.com>
2026-05-27 20:07:05 +0300

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,13 +4,13 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://qabnix.vercel.app/ar",
+      url: "https://qabnix.web.app/ar",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://qabnix.vercel.app/en",
+      url: "https://qabnix.web.app/en",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true, // هذا سيجعل الروابط تنتهي بـ / مما يسهل على فايربيز التعامل معها
+  images: {
+    unoptimized: true,
+  },
 }
 module.exports = nextConfig
